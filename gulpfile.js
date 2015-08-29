576cb52a01ed016342fccd3621c18cccefe36d77
Erik Axelsson <erikaxelsson1@gmail.com>
2015-08-29 15:29:17 +0200

    add readme gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var connect = require('gulp-connect-multi')();
+var sass    = require('gulp-sass');
+
+
+gulp.task('build', function () {
+    return gulp.src(['src/Tiles/TileItem.js','src/Tiles/*.js','src/Ship.js','src/test.js'])
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
+});
+gulp.task('watch', function () {
+
+    gulp.start('connect');
+    gulp.start('build');
+    gulp.start('scss');
+
+    gulp.watch('src/**/*.js', ['build']);
+    gulp.watch('css/*.scss', ['scss']);
+});
+
+gulp.task('scss', function () {
+    return gulp.src(['css/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(connect.reload());
+});
+
+
+gulp.task('connect', connect.server({
+    root: ['dist','bower_components'],
+    port: 1337,
+    livereload: true,
+    open: {
+        file: 'index.html',
+        browser: 'chromium-browser' // if not working OS X browser: 'Google Chrome'
+    }
+}));
